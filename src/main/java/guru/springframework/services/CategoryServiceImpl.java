271f9e00cd3bef55a8358b03f16b876ecb102983
todd <toddtran9@gmail.com>
2018-06-01 16:45:18 -0700

    adding annotation for proper wiring.

--- src/main/java/guru/springframework/services/CategoryServiceImpl.java
+++ src/main/java/guru/springframework/services/CategoryServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Service
 public class CategoryServiceImpl implements CategoryService {
 
     private final CategoryMapper categoryMapper;
