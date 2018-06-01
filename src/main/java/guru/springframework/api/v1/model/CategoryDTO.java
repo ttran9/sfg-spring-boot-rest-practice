02fbe1914de252e9ec02eda8c1a661c508ad15e4
todd <toddtran9@gmail.com>
2018-06-01 16:04:01 -0700

    creating a model to be used with the mapper

--- src/main/java/guru/springframework/api/v1/model/CategoryDTO.java
+++ src/main/java/guru/springframework/api/v1/model/CategoryDTO.java
@@ -1,8 +1,11 @@
 package guru.springframework.api.v1.model;
 
+import lombok.Data;
+
 /**
  * Created by jt on 9/24/17.
  */
+@Data
 public class CategoryDTO {
     private Long id;
     private String name;
