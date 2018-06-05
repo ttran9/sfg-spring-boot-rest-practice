46809978e10895b2af0ee05c12f986f4933d3cf3
todd <toddtran9@gmail.com>
2018-06-05 12:38:30 -0700

    adding in reference code to using the @JsonProperty annotation.

--- src/main/java/guru/springframework/api/v1/model/CustomerDTO.java
+++ src/main/java/guru/springframework/api/v1/model/CustomerDTO.java
@@ -1,10 +1,14 @@
 package guru.springframework.api.v1.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class CustomerDTO {
     private String firstname;
     private String lastname;
+
+    // in the video JT had this @JsonProperty, I am commenting it out just to have it as reference material!
+    // @JsonProperty("customerUrl")
     private String customer_url;
 }
