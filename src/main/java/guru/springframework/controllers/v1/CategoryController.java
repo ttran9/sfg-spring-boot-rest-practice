6f3182a6f9c75277c8f51e58dfbb41eb960f912b
todd <toddtran9@gmail.com>
2018-06-05 15:20:47 -0700

    using @RestController annotation.

--- src/main/java/guru/springframework/controllers/v1/CategoryController.java
+++ src/main/java/guru/springframework/controllers/v1/CategoryController.java
@@ -6,11 +6,9 @@ import guru.springframework.services.CategoryService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequestMapping({CategoryController.BASE_URL})
 //@RequestMapping("${some.url.value}") // using properties file. (the spring context is necessary so unit tests becoming IT texts b/c the spring context would need to be loaded).
 public class CategoryController {
@@ -23,16 +21,14 @@ public class CategoryController {
     }
 
     @GetMapping
-    public ResponseEntity<CategoryListDTO> getAllCategories(){
-
-        return new ResponseEntity<CategoryListDTO>(
-                new CategoryListDTO(categoryService.getAllCategories()), HttpStatus.OK);
+    @ResponseStatus(HttpStatus.OK)
+    public CategoryListDTO getAllCategories(){
+        return new CategoryListDTO(categoryService.getAllCategories());
     }
 
     @GetMapping("/{name}")
-    public ResponseEntity<CategoryDTO> getCategoryByName(@PathVariable String name){
-        return new ResponseEntity<CategoryDTO>(
-                categoryService.getCategoryByName(name), HttpStatus.OK
-        );
+    @ResponseStatus(HttpStatus.OK)
+    public CategoryDTO getCategoryByName(@PathVariable String name){
+        return categoryService.getCategoryByName(name);
     }
 }
