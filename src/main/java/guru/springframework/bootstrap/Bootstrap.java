eaba97c3f3f3b530ceac8f11f5358525e334443d
todd <toddtran9@gmail.com>
2018-06-11 23:41:53 -0700

    Creating sample vendors.

--- src/main/java/guru/springframework/bootstrap/Bootstrap.java
+++ src/main/java/guru/springframework/bootstrap/Bootstrap.java
@@ -2,8 +2,10 @@ package guru.springframework.bootstrap;
 
 import guru.springframework.domain.Category;
 import guru.springframework.domain.Customer;
+import guru.springframework.domain.Vendor;
 import guru.springframework.repositories.CategoryRepository;
 import guru.springframework.repositories.CustomerRepository;
+import guru.springframework.repositories.VendorRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,16 +14,20 @@ public class Bootstrap implements CommandLineRunner {
 
     private CategoryRepository categoryRepository;
     private CustomerRepository customerRepository;
+    private VendorRepository vendorRepository;
 
-    public Bootstrap(CategoryRepository categoryRepository, CustomerRepository customerRepository) {
+    public Bootstrap(CategoryRepository categoryRepository, CustomerRepository customerRepository,
+                     VendorRepository vendorRepository) {
         this.categoryRepository = categoryRepository;
         this.customerRepository = customerRepository;
+        this.vendorRepository = vendorRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
         createCategories();
         createCustomers();
+        createVendors();
     }
 
     private void createCategories() {
@@ -53,17 +59,29 @@ public class Bootstrap implements CommandLineRunner {
         Customer customer = new Customer();
         customer.setFirstname("Joe");
         customer.setLastname("Smith");
-        customer.setId(1L);
+//        customer.setId(1L);
 
         customerRepository.save(customer);
 
         Customer customerTwo = new Customer();
         customerTwo.setFirstname("Bob");
         customerTwo.setLastname("Franco");
-        customerTwo.setId(2L);
+//        customerTwo.setId(2L);
 
         customerRepository.save(customerTwo);
 
         System.out.println("Customers loaded = " + customerRepository.count());
+    }
+
+    private void createVendors() {
+        Vendor vendor = new Vendor();
+        vendor.setName("Coca Cola Solutions");
+        vendorRepository.save(vendor);
+
+        Vendor vendorTwo = new Vendor();
+        vendor.setName("Dell Solutions");
+        vendorRepository.save(vendorTwo);
+
+        System.out.println("Vendors loaded = " + vendorRepository.count());
     }
 }
